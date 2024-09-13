f016b1605f3b6543b94a2daf173fa91d359cf8c2
Akshat-hype <akshatgoel310805@gmail.com>
2024-09-14 00:07:07 +0530

    login page done

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import logo from './cropped-android-chrome-384x384-1-1.webp'; // Update the path if needed
 import './App.css';
+import '@fontsource/inter';
+import JoySignInSideTemplate from './JoySignInSideTemplate';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <JoySignInSideTemplate/>
   );
 }
 
